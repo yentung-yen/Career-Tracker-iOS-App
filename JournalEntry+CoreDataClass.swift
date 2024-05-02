e388ff69378c1bb8e666ebf2ef5100a2fd17934e
Yen Tung Chin <ychi0036@student.monash.edu>
2024-05-02 14:10:32 +1000

    [implemented] core data controller for journal entries, [add] journal entry entity to datamodel

---
+++ JournalEntry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  JournalEntry+CoreDataClass.swift
+//  jobApplication_prepTrack_app
+//
+//  Created by Chin Yen Tung on 2/5/2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(JournalEntry)
+public class JournalEntry: NSManagedObject {
+
+}
---
+++ JournalEntry+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  JournalEntry+CoreDataProperties.swift
+//  jobApplication_prepTrack_app
+//
+//  Created by Chin Yen Tung on 2/5/2024.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension JournalEntry {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<JournalEntry> {
+        return NSFetchRequest<JournalEntry>(entityName: "JournalEntry")
+    }
+
+    @NSManaged public var journalEntryTitle: String?
+    @NSManaged public var journalEntryDate: String?
+    @NSManaged public var journalEntryCategories: [String]?
+    @NSManaged public var journalEntryDesc: String?
+
+}
+
+extension JournalEntry : Identifiable {
+
+}

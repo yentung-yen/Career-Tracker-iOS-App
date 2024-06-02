cb693ee8f1d31dbc1c2e0fb52272285e645c3cf4
Yen Tung Chin <ychi0036@student.monash.edu>
2024-06-03 00:21:24 +1000

    [updated] CreateJournalEntryVC to show categories as collection view and save selected cells into the categoriesList in firebase

--- fit3178_majorAssignment/JournalEntry/CreateJournalEntryViewController.swift
+++ fit3178_majorAssignment/JournalEntry/CreateJournalEntryViewController.swift
@@ -9,36 +9,51 @@ import UIKit
 
 class CreateJournalEntryViewController: UIViewController {
 
+    @IBOutlet weak var categoriesCollectionView: UICollectionView!
+    
     @IBOutlet weak var entryTitleTextField: UITextField!
     @IBOutlet weak var entryDatePicker: UIDatePicker!
-    @IBOutlet weak var entryCategories: UITextField!
-    @IBOutlet weak var entryDescTextField: UITextField!
+    @IBOutlet weak var entryDescTextField: UITextView!
     
     weak var databaseController: DatabaseProtocol?
-    var categoryList: [String] = []
+    var allCategoryList = ["Leadership", "Communication", "Adaptability", "Time Management", "Problem Solving", "Teamwork"]
+    var arrSelectedIndex = [IndexPath]() // to store selected cell Index array
+    var arrSelectedCategoryData = [String]() // To store selected cell category data array
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // add borders to entry desc text view
+        entryDescTextField!.layer.borderWidth = 1
+        entryDescTextField!.layer.borderColor = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.00).cgColor
+        entryDescTextField!.layer.cornerRadius = 5.0
 
         // Do any additional setup after loading the view.
         let appDelegate = UIApplication.shared.delegate as? AppDelegate // get access to the AppDelegate
         databaseController = appDelegate?.firebaseDatabaseController    // store a reference to the databaseController
+        
+        // setup collection view
+        categoriesCollectionView.dataSource = self
+        categoriesCollectionView.delegate = self
+        
+        // set collection view layout
+        categoriesCollectionView.setCollectionViewLayout(UICollectionViewCompositionalLayout(section: createCategoriesLayout()), animated: false)
     }
     
     @IBAction func onAddEntryClick(_ sender: Any) {
         //TODO: validate date picker
         // validate the user input
-        guard let title = entryTitleTextField.text, let category = entryCategories.text, let desc = entryDescTextField.text else {
+        guard let title = entryTitleTextField.text, let desc = entryDescTextField.text else {
             return
         }
         // - ensure name and abilities are not empty
-        if title.isEmpty || category.isEmpty || desc.isEmpty {
+        if title.isEmpty || desc.isEmpty || arrSelectedIndex.count == 0{
             var errorMsg = "Please ensure all fields are filled:\n"
             
             if title.isEmpty {
                 errorMsg += "- Title must be provided\n"
             }
-            if category.isEmpty {
+            if arrSelectedIndex.count == 0 {
                 errorMsg += "- Category must be provided\n"
             }
             if desc.isEmpty {
@@ -54,10 +69,7 @@ class CreateJournalEntryViewController: UIViewController {
         dateFormatter.dateFormat = "dd-MMM-yyyy" // set date format
         let dateString = dateFormatter.string(from: entryDatePicker.date) // convert to string
         
-        // TODO: handle category list
-        categoryList.append(category)
-        
-        let _ = databaseController?.addJournalEntry(entryTitle: title, entryDate: dateString, entryCategories: categoryList, entryDes: desc)
+        let _ = databaseController?.addJournalEntry(entryTitle: title, entryDate: dateString, entryCategories: arrSelectedCategoryData, entryDes: desc)
         navigationController?.popViewController(animated: true)
     }
     
@@ -70,5 +82,73 @@ class CreateJournalEntryViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: collection view layout
+    
+    func createCategoriesLayout() -> NSCollectionLayoutSection {
+        let fixedHeight: CGFloat = 30 // create var for fixed cell height
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(fixedHeight))
+        let itemLayout = NSCollectionLayoutItem(layoutSize: itemSize)
+        // Add padding around each item
+        itemLayout.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 2, bottom: 0, trailing: 2)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(fixedHeight))
+        let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [itemLayout])
+        
+        let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+        layoutSection.interGroupSpacing = 5
+        
+        //layoutSection.orthogonalScrollingBehavior = .continuous
+        return layoutSection
+    }
+}
 
+// MARK: Collection view controller
+
+// reference - https://stackoverflow.com/questions/52757524/how-do-i-got-multiple-selections-in-uicollection-view-using-swift-4
+extension CreateJournalEntryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.allCategoryList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "journalCategoryCollectionViewCellIdentifier", for: indexPath) as! JournalCategoryCollectionViewCell
+        
+        // add category labels to each cell
+        cell.categoryLabel.text = allCategoryList[indexPath.row]
+        
+        if arrSelectedIndex.contains(indexPath) {
+            // if user has selected a cell, change the cell colour
+            // #999999 colour
+            cell.backgroundColor = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.00)
+        }
+        else {
+            // #D9D9D9 colour
+            cell.backgroundColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.00)
+        }
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("You selected cell index #\(indexPath.item)!")
+//        print("You selected cell #\(allCategoryList[indexPath.item])")
+        
+        let selectedCategory = allCategoryList[indexPath.item]
+
+        // check if item is already selected, if already selected:
+        if arrSelectedIndex.contains(indexPath) {
+            // deselect it by filtering arrSelectedIndex & arrSelectedData lists
+            // to only include items that are != indexPath and != selectedCategory
+            // which creates 2 new lists that doesnt include those items
+            arrSelectedIndex = arrSelectedIndex.filter { $0 != indexPath}
+            arrSelectedCategoryData = arrSelectedCategoryData.filter { $0 != selectedCategory}
+        }
+        else {
+            // add selected category to arrSelectedIndex and arrSelectedData lists
+            arrSelectedIndex.append(indexPath)
+            arrSelectedCategoryData.append(selectedCategory)
+        }
+        collectionView.reloadData()
+    }
 }

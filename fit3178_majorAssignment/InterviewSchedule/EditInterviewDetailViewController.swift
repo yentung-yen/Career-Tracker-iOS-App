a2b7c980ec89934eab6700501a454766bc16f30d
Yen Tung Chin <ychi0036@student.monash.edu>
2024-06-08 05:04:54 +1000

    [implemented] editInterviewDetailVC to edit interview data and save new changes

--- fit3178_majorAssignment/InterviewSchedule/EditInterviewDetailViewController.swift
+++ fit3178_majorAssignment/InterviewSchedule/EditInterviewDetailViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class EditInterviewDetailViewController: UIViewController {
+    var currentInterviewDetails: InterviewScheduleDetail?
+    
+    var objectID: NSManagedObjectID?
+    var managedObjectContext: NSManagedObjectContext?
+    weak var databaseController: DatabaseProtocol?
 
     @IBOutlet weak var interviewTextField: UITextField!
     @IBOutlet weak var interviewStartDatePicker: UIDatePicker!
@@ -21,9 +27,47 @@ class EditInterviewDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.firebaseDatabaseController
+        managedObjectContext = databaseController?.persistentContainer.viewContext
+        objectID = currentInterviewDetails?.objectID
+        
+        // set initial data onto UI
+        interviewTextField.text = currentInterviewDetails?.interviewTitle
+        interviewStartDatePicker.date = (currentInterviewDetails?.interviewStartDatetime)!
+        interviewEndDatePicker.date = (currentInterviewDetails?.interviewEndDatetime)!
+        interviewVidConference.text = currentInterviewDetails?.interviewVideoLink
+        interviewLocation.text = currentInterviewDetails?.interviewLocation
+        interviewNotifDatePicker.date = (currentInterviewDetails?.interviewNotifDatetime)!
+        interviewNotes.text = currentInterviewDetails?.interviewNotes
     }
     
-
+    @IBAction func onSaveChangesClick(_ sender: Any) {
+        // create new InterviewScheduleDetail object with the same objectID as the one we want to edit
+        if let object = managedObjectContext?.object(with: objectID!) as? InterviewScheduleDetail {
+            object.interviewTitle = interviewTextField.text
+            object.interviewStartDatetime = interviewStartDatePicker.date
+            object.interviewEndDatetime = interviewEndDatePicker.date
+            object.interviewVideoLink = interviewVidConference.text
+            object.interviewLocation = interviewLocation.text
+            object.interviewNotifDatetime =  interviewNotifDatePicker.date
+            object.interviewNotes = interviewNotes.text
+        }
+        
+        // save the context
+        do {
+            try managedObjectContext?.save()
+            
+            // TODO: reload data in viewDataVC instead of move to main screen
+            // move to main interview schedule screen
+            navigationController?.popViewController(animated: true)
+            navigationController?.popViewController(animated: true)
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Failed to save context: \(error)")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
--- fit3178_majorAssignment/InterviewSchedule/ViewInterviewDetailViewController.swift
+++ fit3178_majorAssignment/InterviewSchedule/ViewInterviewDetailViewController.swift
@@ -64,14 +64,20 @@ class ViewInterviewDetailViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "viewToEditInterviewSegue" {
+            if let destinationVC = segue.destination as? EditInterviewDetailViewController {
+                
+                destinationVC.currentInterviewDetails = currentInterviewDetails
+            }
+        }
     }
-    */
+    
 
 }
